31774fbc078de4b53305cc355fbba16678d0029d
Eryn Wells <eryn@erynwells.me>
2015-07-18 10:39:53 -0700

    Basic unit test for unadvanced Rotor

--- EnigmaTests/EnigmaTests.swift
+++ EnigmaTests/EnigmaTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+@testable import Enigma
+
 
 class EnigmaTests: XCTestCase {
     
@@ -32,4 +34,24 @@ class EnigmaTests: XCTestCase {
         }
     }
     
+}
+
+
+class RotorTests: XCTestCase {
+    let rotorSeries = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+    var rotor: Enigma.Rotor!
+
+    override func setUp() {
+        rotor = try! Enigma.Rotor(series: rotorSeries)
+    }
+
+    override func tearDown() {
+        rotor = nil
+    }
+
+    func testThatUnadvancedSubstitutionWorks() {
+        for (plainCharacter, cipherCharacter) in zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ".characters, rotorSeries.characters) {
+            XCTAssertEqual(try! rotor.encode(plainCharacter), cipherCharacter)
+        }
+    }
 }

f461fbf23783d335d71b37ed594811884f84bdf2
Eryn Wells <eryn@erynwells.me>
2015-07-20 21:12:16 -0700

    MachineViewController

---
+++ Enigma/MachineViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MachineViewController.swift
+//  Enigma
+//
+//  Created by Eryn Wells on 2015-07-18.
+//  Copyright © 2015 Eryn Wells. All rights reserved.
+//
+
+import UIKit
+
+class MachineViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - UITextFieldDelegate
+}
+

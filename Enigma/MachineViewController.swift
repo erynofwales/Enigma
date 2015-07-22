c96c0bd0fcb265d06e29a968223b53cd58523714
Eryn Wells <eryn@erynwells.me>
2015-07-21 20:21:54 -0700

    Rename ViewController -> MachineViewController

--- Enigma/MachineViewController.swift
+++ Enigma/MachineViewController.swift
@@ -14,8 +14,54 @@ class MachineViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputTextField.delegate = self
+        //outputTextField.delegate = self
     }
 
     // MARK: - UITextFieldDelegate
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        print("textFieldDidEndEditing")
+    }
+
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing")
+        return true
+    }
+
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        print("textFieldShouldEndEditing")
+        return true
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField === inputTextField {
+            var machine: Machine! = nil
+            do {
+                machine = Machine(rotors: [try Rotor(.EnigmaI), try Rotor(.EnigmaII), try Rotor(.EnigmaIII)], reflector: try Reflector(.EnigmaB), plugboard: Plugboard())
+            } catch let error {
+                print("Error setting up machine: \(error)")
+                return true
+            }
+            if let inputText = textField.text {
+                do {
+                    outputTextField.text = try machine.encode(inputText)
+                } catch let error {
+                    print("Error encoding: \(error)")
+                    return true
+                }
+            }
+            return false
+        }
+        return true
+    }
+
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        print("textFieldShouldClear")
+        if textField === inputTextField {
+            return true
+        }
+        return false
+    }
 }
 
